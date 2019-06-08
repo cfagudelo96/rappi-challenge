ee897dcae4cc035967603051f94e283692a30f9c
Carlos Felipe Agudelo Ospina <cf.agudelo96@gmail.com>
2019-06-07 19:31:30 -0500

    Configuración inicial ambiente dev

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'brtn-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
